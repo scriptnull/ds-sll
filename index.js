fb779090e26dad27fd24ddfb54196349cd6590ef
scriptnull <vishnubharathi@live.com>
2015-12-11 12:50:40 +0530

    initial commit
    Basic implementation along with tests

---
+++ index.js
@@ -0,0 +1,168 @@
+module.exports = (function(){
+	
+	var Node = function(data){
+		this.data = data ;
+		this.next = null ;
+	};
+
+	Node.prototype.setNext = function (node) {
+		this.next = node ;
+	};
+
+	Node.prototype.getNext = function(){
+		return this.next;
+	};	
+
+	var LinkedList = function(){
+			this.length = 0 ;
+			this.head = null ;
+	};
+	
+	LinkedList.prototype.show = function(){
+		var str = [];
+		var temp = this.head ;
+		if(!this.head)return '';
+		while(temp.getNext()){
+			str.push(JSON.stringify(temp.data || {} ));
+			temp = temp.getNext();
+		}
+		str.push(JSON.stringify(temp.data || {} ));
+		var result = str.join(' -> ');
+		//console.log(result);
+		return result;
+	};
+	
+	LinkedList.prototype.showLength = function(){
+		console.log(this.length);
+		return 	this.length;
+	};
+
+	LinkedList.prototype.insertAtHead = function(node){
+		this.length++;
+		if(!this.head){
+			this.head = node;
+			return;
+		}else{
+			var temp = this.head ;
+			this.head = node ;
+			this.head.setNext(temp);
+		}
+	};
+	
+	LinkedList.prototype.insertAtTail = function(node){
+		this.length++;
+		if(!this.head){
+			this.head = node;
+			return ;
+		}else{
+			var temp = this.head;
+			while(temp.getNext()){
+				temp = temp.getNext();
+			}
+			node.setNext(null);
+			temp.setNext(node);
+		}
+	};
+	
+	LinkedList.prototype.insertAtPosition = function(node , position){
+		if(position <= 1 ){
+			this.insertAtHead(node);
+			return;
+		}
+		if(position > this.length){
+			this.insertAtTail(node);
+			return;		
+		}
+		else{
+			var pos = 1 , temp = this.head , prev ;
+			while(temp.getNext()){
+				prev = temp;
+				temp = temp.getNext();
+				pos++;
+				if(pos === position){
+					this.length++;
+					prev.setNext(node);
+					node.setNext(temp);
+					break;
+				}
+			}
+		}
+	};
+	
+	LinkedList.prototype.deleteAtHead = function(){
+		if(this.head){
+			if(this.head.getNext()){
+				this.head = this.head.getNext();
+				this.length--;
+			}
+		}
+	};
+	
+	LinkedList.prototype.deleteAtTail = function(){
+		if(this.head){
+			var temp = this.head , prev ;
+			while(temp.getNext()){
+				prev = temp;
+				temp = temp.getNext();
+			}
+			prev.setNext(null);
+			this.length--;
+		}
+	};
+	
+	
+	LinkedList.prototype.deleteAtPosition = function(position){
+		if(position <= 0 )
+			position = 1 ;
+		if(position > this.length )
+			position = this.length;
+		if(position === 1 ){
+			this.deleteAtHead();
+			return;
+		}else if(position === this.length){
+			this.deleteAtTail();
+			return;
+		}else{
+			var pos = 1 , temp = this.head , prev ;
+			while(temp.getNext()){
+				prev = temp ;
+				temp = temp.getNext();
+				pos++;
+				if(pos === position){
+					prev.setNext(temp.getNext());
+					this.length--;
+					break;					
+				}
+			}
+		}
+	};
+	
+	LinkedList.prototype.getDataAtPosition = function(position){
+		if(position <= 1 ) {
+			return this.head;
+		}
+		if(position > this.length ) position = this.length;
+		var pos = 2 , temp = this.head ;
+		while(temp = temp.getNext()){
+			if(pos === position){
+				return temp.data;
+			}
+			pos++;
+		}  
+	};
+	
+	LinkedList.prototype.findPositionByData = function(data){
+		var temp = this.head;
+		for(var i = 1 ; i <= this.length ; i++){
+			if(temp.data === data )return i ;
+			temp = temp.getNext();
+		}
+		return -1;		
+	};
+	
+	return {
+		LinkedList : LinkedList ,
+		Node : Node
+	};
+
+})();
---
+++ test.js
@@ -0,0 +1,331 @@
+var ds = require('./index.js')
+ , LinkedList = ds.LinkedList
+ , Node = ds.Node 
+ , assert = require('assert');
+ 
+
+describe('Node' , function(){
+	
+	describe('init' , function(){
+		it('should create a Node instance' , function(){
+			assert.ok(new Node(1));	
+		});
+		
+		it('should create node with no data' , function(){
+			assert.ok(!new Node().data);
+		});
+		
+		it('should create node with object type data' , function(){
+			assert.ok(typeof new Node({}).data === 'object');
+		});
+	});
+	
+	describe('setters and getters' , function(){
+		var a , b ;
+		before(function(){
+			a = new Node(1);
+			b = new Node(2);
+		});
+		
+		it('should set the next node for current node', function(){
+			a.setNext(b);
+			assert.ok(a.next === b);
+		});
+		
+		it('should get the next node for current node' , function(){
+			assert.ok(a.getNext() === b);
+		});
+	});
+	
+});
+
+describe('Linked List' , function(){
+	
+	describe('init' , function(){
+		
+		it('should create a Linked List instance' , function(){
+			var ll = new LinkedList();
+			assert.ok(ll);
+			assert.ok(ll.length === 0);
+			assert.ok(ll.head === null);
+		});
+		
+	});
+	
+	describe('show()' , function(){
+		var ll , emptyList ;
+		before(function(){
+			ll = new LinkedList();
+			var a = new Node(1);
+			var b = new Node(2);
+			var c = new Node(3);
+			a.setNext(b);
+			b.setNext(c);
+			ll.head = a ;
+			emptyList = new LinkedList();
+		});
+		it('should visualize empty Linked List' , function(){
+			assert.ok(emptyList.show() === '');
+		});
+		it('should visualize Linked List with one node' , function(){
+			emptyList.head = new Node(1);
+			assert.ok(emptyList.show() === '1');
+		});
+		it('should visualize the Linked List' , function(){
+			assert.ok(ll.show() === '1 -> 2 -> 3');
+		});
+	});
+	
+	describe('insertAtHead()' , function(){
+		var ll ;
+		before(function(){
+			ll = new LinkedList();
+		});
+		
+		it('should insert node at head' , function(){
+			var node = new Node(1);
+			ll.insertAtHead(node);
+			
+			assert.ok(ll.length === 1);
+			assert.ok(ll.head === node);
+			assert.ok(ll.head.getNext() === null);
+		});
+		
+		it('should insert node before head' , function(){
+			var node = new Node(2);
+			ll.insertAtHead(node);
+			
+			assert.ok(ll.length === 2);
+			assert.ok(ll.head === node);
+			assert.ok(ll.head.getNext() !== null);
+		});
+		
+		it('should check visually' , function(){
+			assert.ok(ll.show() === '2 -> 1');
+		});
+			
+	});
+	
+	describe('insertAtTail()' , function(){
+		var ll ;
+		before(function(){
+			ll = new LinkedList();
+		});
+		
+		it('should insert node at tail' , function(){
+			var node = new Node(1);
+			ll.insertAtTail(node);
+			
+			assert.ok(ll.length === 1);
+			assert.ok(ll.head === node);
+			assert.ok(ll.head.getNext() === null);
+		});
+		
+		it('should insert node after tail' , function(){
+			var node = new Node(2);
+			ll.insertAtTail(node);
+			
+			assert.ok(ll.length === 2);
+			assert.ok(ll.head.next === node);
+		});
+		
+		it('should check visually' , function(){
+			assert.ok(ll.show() === '1 -> 2');
+		});
+			
+	});
+	
+	
+	describe('insertAtPosition()' , function(){
+		var ll ;
+		before(function(){
+			ll = new LinkedList();
+		});
+		
+		it('should insert node at head' , function(){
+			var node = new Node(1);
+			ll.insertAtPosition(node , 0 );
+			
+			assert.ok(ll.length === 1);
+			assert.ok(ll.head === node);
+			assert.ok(ll.head.getNext() === null);
+		});
+		
+		it('should insert at tail' , function(){
+			var node = new Node(2);
+			ll.insertAtPosition(node , 2);
+			
+			assert.ok(ll.length === 2);
+			assert.ok(ll.head.next === node);
+		});
+		
+		it('should insert in middle' , function(){
+			var node = new Node(3);
+			ll.insertAtPosition(node , 2);
+			
+			assert.ok(ll.length === 3);
+		});
+		
+		it('should check visually' , function(){
+			assert.ok(ll.show() === '1 -> 3 -> 2');
+		});
+			
+	});
+	
+	describe('deleteAtHead()' , function(){
+		var ll , a , b , c;
+		before(function(){
+			ll = new LinkedList();
+			a = new Node(1);
+			b = new Node(2);
+			c = new Node(3);
+			ll.insertAtTail(a);
+			ll.insertAtTail(b);
+			ll.insertAtTail(c);
+		});
+		
+		it('should delete head node' , function(){
+			ll.deleteAtHead();
+			
+			assert.ok(ll.length === 2);
+			assert.ok(ll.head === b);
+		});
+		
+		it('should check visually' , function(){
+			assert.ok(ll.show() === '2 -> 3');
+		});
+	});
+	
+	describe('deleteAtTail()' , function(){
+		var ll , a , b , c;
+		before(function(){
+			ll = new LinkedList();
+			a = new Node(1);
+			b = new Node(2);
+			c = new Node(3);
+			ll.insertAtTail(a);
+			ll.insertAtTail(b);
+			ll.insertAtTail(c);
+		});
+		
+		it('should delete tail node' , function(){
+			ll.deleteAtTail();
+			
+			assert.ok(ll.length === 2);
+			assert.ok(ll.head.next.next === null);
+		});
+		
+		it('should check visually' , function(){
+			assert.ok(ll.show() === '1 -> 2');
+		});
+	});
+	
+	describe('deleteAtPosition()' , function(){
+
+		context('when Linked List is empty' , function(){
+			var ll , a , b , c;
+			before(function(){
+				ll = new LinkedList();
+			});
+			
+			it('should not delete anything' , function(){
+				ll.deleteAtPosition(1);
+			});
+			
+		});
+
+		
+		context('when Linked List is not empty' , function(){
+			var ll , a , b , c;
+			before(function(){
+				ll = new LinkedList();
+				a = new Node(1);
+				b = new Node(2);
+				c = new Node(3);
+				ll.insertAtTail(a);
+				ll.insertAtTail(b);
+				ll.insertAtTail(c);
+			});
+			
+			it('should delete head node' , function(){
+				ll.deleteAtPosition(0);	
+				assert.ok(ll.length === 2);
+			});
+			it('should check visually' , function(){
+				assert.ok(ll.show() === '2 -> 3');
+			});
+			it('should delete head node' , function(){
+				ll.deleteAtPosition(2);	
+				assert.ok(ll.length === 1);
+			});
+			it('should check visually' , function(){
+				assert.ok(ll.show() === '2');
+			});
+		});
+			
+	});
+	
+	describe('getDataAtPosition()' , function(){
+		var ll , emptyList ;
+		before(function(){
+			ll = new LinkedList();
+			emptyList = new LinkedList();
+			
+			a = new Node(1);
+			b = new Node(2);
+			c = new Node(3);
+			ll.insertAtTail(a);
+			ll.insertAtTail(b);
+			ll.insertAtTail(c);
+		});
+		
+		context('when Linked List is empty' , function(){
+			it('should return null' , function(){
+				assert.ok(emptyList.getDataAtPosition(1) === null);
+			});
+		});
+		
+		context('when Linked List is not empty' , function(){
+			it('should return correct data' , function(){
+				assert.ok(ll.getDataAtPosition(1) , 1);
+				assert.ok(ll.getDataAtPosition(2) , 2);
+				assert.ok(ll.getDataAtPosition(3) , 3);
+			});
+		});
+		
+	});
+	
+	describe('findPositionByData()' , function(){
+		var ll , emptyList ;
+		before(function(){
+			ll = new LinkedList();
+			emptyList = new LinkedList();
+			
+			a = new Node(1);
+			b = new Node(2);
+			c = new Node(3);
+			ll.insertAtTail(a);
+			ll.insertAtTail(b);
+			ll.insertAtTail(c);
+		});
+		
+		context('when Linked List is empty' , function(){
+			it('should return -1' , function(){
+				assert.ok(emptyList.findPositionByData(1) === -1);
+			});
+		});
+		
+		context('when Linked List is not empty' , function(){
+			it('should return correct position' , function(){
+				assert.ok(ll.findPositionByData(1) , 1 );
+				assert.ok(ll.findPositionByData(2) , 2);
+				assert.ok(ll.findPositionByData(3) , 3);
+			});
+			it('should return -1 if data not found' , function(){
+				assert.ok(ll.findPositionByData(4) , -1);
+			});
+		});
+		
+	});
+	
+});
